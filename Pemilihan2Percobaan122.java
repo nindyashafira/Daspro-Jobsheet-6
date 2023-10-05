9dccb5b53b558f1ab45e35a8103bf4d6103db152
nindyashafira <nindyashafira24@gmail.com>
2023-10-05 13:46:12 +0700

    Daspro Jobsheet 6_Nindya Shafira Putri_22

--- Pemilihan2Percobaan122.java
+++ Pemilihan2Percobaan122.java
@@ -2,15 +2,13 @@ import java.util.Scanner;
 public class Pemilihan2Percobaan122 {
 public static void main(String[] args) {
     Scanner input22 = new Scanner(System.in);
-    int tahun;
     System.out.print("Masukkan tahun: ");
-    tahun = input22.nextInt();
+    int tahun = input22.nextInt();
 
-    if ((tahun % 4) == 0) {
-        if ((tahun % 100) != 0)
-        System.out.println("Tahun Kabisat");
-    }else
-    System.out.println("Bukan Tahun Kabisat");
-
+    if ((tahun % 4) == 0 && tahun % 100 != 0) {
+        System.out.println(tahun + " adalah tahun kabisat");
+    }else {
+        System.out.println(tahun + " bukan tahun kabisat");
+    }
 }
 }
