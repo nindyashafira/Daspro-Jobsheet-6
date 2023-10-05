63cce12eb721be6bb1e43963539d5d0c6722420a
nindyashafira <nindyashafira24@gmail.com>
2023-10-05 14:34:04 +0700

    Daspro Jobsheet 6_Nindya Shafira Putri_22

--- Pemilihan2Percobaan222.java
+++ Pemilihan2Percobaan222.java
@@ -14,10 +14,10 @@ public class Pemilihan2Percobaan222 {
         totalSudut = sudut1 + sudut2 + sudut3;
         if (totalSudut == 180) {
             if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90))
-            System.out.println("Segitiga tersebut adalah segitiga siku-siku");
-            else
-            System.out.println("Segitiga tersebut adalah bukan segitiga siku-siku");
-        } else
+                System.out.println("Segitiga tersebut adalah segitiga siku-siku");
+                else
+                System.out.println("Segitiga tersebut adalah bukan segitiga siku-siku");
+        }else
         System.out.println("Bukan Segitiga");
     }
 }
