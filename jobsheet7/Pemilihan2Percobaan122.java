c370165fab836debebed98b7cee4e99634044991
nindyashafira <nindyashafira24@gmail.com>
2023-10-30 14:34:55 +0700

    pemilihan 2

---
+++ jobsheet7/Pemilihan2Percobaan122.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Pemilihan2Percobaan122 {
+public static void main(String[] args) {
+    Scanner input22 = new Scanner(System.in);
+    int tahun;
+    System.out.print("Masukkan tahun: ");
+    tahun = input22.nextInt();
+
+    if ((tahun % 4) == 0) {
+        if ((tahun % 100) != 0)
+        System.out.println("Tahun Kabisat");
+    }else
+    System.out.println("Bukan Tahun Kabisat");
+}
+}
