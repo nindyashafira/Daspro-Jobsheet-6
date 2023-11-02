fb1fa397d67ae61c780fb816d20e8ae35be77450
nindyashafira <nindyashafira24@gmail.com>
2023-11-02 13:53:08 +0700

    Modif Bioskop-2

--- Bioskop22.java
+++ Bioskop22.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Bioskop22{
     public static void main(String[] args) {
         String[][] penonton = new String[4][2];
@@ -17,7 +19,7 @@ public class Bioskop22{
                 System.out.println("Panjang baris: " + barisPenonton.length);
             }
         }
-
+        
         System.out.printf("%s \t %s \n", penonton[0][0], penonton[0][1]);
         System.out.printf("%s \t %s \n", penonton[1][0], penonton[1][1]);
         System.out.printf("%s \t %s \n", penonton[2][0], penonton[2][1]);
@@ -28,5 +30,11 @@ public class Bioskop22{
         System.out.println(penonton[1].length);
         System.out.println(penonton[2].length);
         System.out.println(penonton[3].length);
+
+       System.out.println("Penonton pada baris ke-3: ");
+        for (int i=0; i<penonton[2].length; i++) {
+            System.out.println(penonton[2][i]);
+        }
+
     }
 }
