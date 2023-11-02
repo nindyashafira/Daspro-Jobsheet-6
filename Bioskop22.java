4414335ba8882f9cdf5f81f29a2cf8b2d7035f26
nindyashafira <nindyashafira24@gmail.com>
2023-11-02 14:08:47 +0700

    Modif Bioskop 4

--- Bioskop22.java
+++ Bioskop22.java
@@ -31,9 +31,8 @@ public class Bioskop22{
         System.out.println(penonton[2].length);
         System.out.println(penonton[3].length);
 
-       System.out.println("Penonton pada baris ke-3: ");
-        for (String i : penonton[2]) {
-            System.out.println(i);
+        for (int i=0; i<penonton.length; i++) {
+            System.out.println("Penonton pada baris ke-" + (i+1) + ": " + String.join(", ", penonton[i]));
         }
 
     }
